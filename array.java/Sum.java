89fd475e3c19f3992161fcb5123e4e43aec94607
Anshul Rathore <anshulrathore9009@gmail.com>
2026-01-06 22:47:47 +0530

    Added Sum.java program and updated LinkedListCycle

---
+++ array.java/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+
+    public static void main(String[] args) {
+        int[] num={1,2,3,4,5};
+        int sum=0;
+        for(int s:num){
+            sum += s;
+        }
+        System.out.println(sum);
+
+    }
+
+}
