214d63ab413ddff5807b83ec18e6c67dc3fc66e7
Anshul Rathore <anshulrathore9009@gmail.com>
2026-01-09 10:34:52 +0530

    count Frequency of string

--- Strings.java/CountFrequency.java
+++ Strings.java/CountFrequency.java
@@ -9,14 +9,14 @@ public class CountFrequency {
         HashMap<Character, Integer> freqMap = new HashMap<>();
 
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch=str.charAt(i);
+       for (int i = 0; i < str.length(); i++) {
+           char ch=str.charAt(i);
 
             freqMap.put(ch, freqMap.getOrDefault(ch, 0)+1);
-        }
-        for(char key:freqMap.keySet()){
-            System.out.println(key + " ->"+freqMap.get(key));
-        }
+       }
+       for(char c:freqMap.keySet()){
+        System.out.println(c + " :"+freqMap.get(c));
+       }
 
         
 
