91a81b990e65e967ff44fa51c87fa893980fe69f
Anshul Rathore <anshulrathore9009@gmail.com>
2026-01-09 10:40:42 +0530

    find Repatind char

--- Strings.java/FindNonRepeationgwords.java
+++ Strings.java/FindNonRepeationgwords.java
@@ -7,18 +7,19 @@ public class FindNonRepeationgwords {
 
     HashMap<Character,Integer> nonrep=new HashMap<>();
 
-    for (int i = 0; i < str.length(); i++) {
-        char ch=str.charAt(i);
-        nonrep.put(ch, nonrep.getOrDefault(ch, 0)+1);
-    }
-    for (int i = 0; i < str.length(); i++) {
-        char ch=str.charAt(i);
-        if(nonrep.get(ch) ==1){
-            System.out.println("First non-repeating character is :"+ch);
-            return;
-        }
-    }
-    System.out.println("NO non-repeating character found");
+   for (int i = 0; i < str.length(); i++) {
+       char ch=str.charAt(i);
+
+       nonrep.put(ch, nonrep.getOrDefault(ch, 0)+1);
+   }
+   for (int i = 0; i < str.length(); i++) {
+       char ch=str.charAt(i);
+       if(nonrep.get(ch)==1){
+        System.out.println(ch);
+        return;
+       }
+   }
+   System.out.println("NO repeating charcter");
 
    
  }   
