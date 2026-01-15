b486328830be7070e301b03c69da1c13a1027e5f
Anshul Rathore <anshulrathore9009@gmail.com>
2026-01-15 10:54:47 +0530

    Stack que

--- StackByCollectionFramework.java/StackQuestions.java/ValidParentheses.java
+++ StackByCollectionFramework.java/StackQuestions.java/ValidParentheses.java
@@ -24,6 +24,6 @@ public class ValidParentheses {
 
     public static void main(String[] args) {
         System.out.println(isvalid("({[]})"));
-        System.out.println(isvalid("()[{}]"));
+        System.out.println(isvalid("()[}]"));
     }
 }
