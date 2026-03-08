35fed24b7a5fd5ee477aa68f91fb6dc4fe50dcf6
Anshul Rathore <anshulrathore9009@gmail.com>
2026-03-08 15:09:50 +0530

    pratice moves all zero at the end

---
+++ 39/MoveZero.java
@@ -0,0 +1,24 @@
+public class MoveZero {
+
+    public static void pushzero(int arr[]){
+        int index=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0){
+                arr[index]=arr[i];
+                index++;
+            }
+        }
+        while(index < arr.length){
+            arr[index]=0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={4,5,0,1,9,0,5,0};
+        pushzero(arr);
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}
